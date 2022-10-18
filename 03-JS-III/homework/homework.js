caa49c0a4dbc3bba91a630b0c1a0aa7b2659d2b2
fede-baigorria <fede.baigorria.1@gmail.com>
2022-10-17 21:06:43 -0300

    finalizado

--- 03-JS-III/homework/homework.js
+++ 03-JS-III/homework/homework.js
@@ -8,29 +8,48 @@ function imprimirSumaNumeros() {
     // Imprime la suma de los números del 1 al 10
     // Pista: usa un acumulador
     // Tu código:
+    var suma = 0 
+    for (i = 0; i < 11; i++) {
+    suma = suma + i
+  }
+  return suma
 }
 
 function encuentraPares(array){
   // Devuelve un arreglo con los pares encontrados
   // en el arreglo de enteros pasado como parámetro
   // Tu código:
+  var resultado = [];
+  var i = 0;
+  while (i < array.length) {
+  if (array [i]  % 2 === 0)  
+  resultado.push (array [i]); i++
+  }
+  return resultado
 }
 
 function elevaAlCuadrado(array){
   // Devuelve un arreglo con cada número del array
   // elevado al cuadrado
   // Tu código:
-}
+  return array.map (function(num){
+    return num ** 2
+    });
+  }
 
 function sumaArray(array){
   // Devuelve el resultado de sumar todos los elementos
   // de un arreglo de enteros dado
   // Tu código:
+  return array.reduce (function(acc,num){
+    return acc + num ;
+  });
 }
 
 function numeroDigitos(num){
   // Devuelve el número de dígitos de un número dado
-  // Tu código:
+  // Tu código: 
+    return ( num.toString ().length); 
 }
   
   
